5fe9ba58fba1936d897c4e3585f648a571efd986
Abdullah Khamis <abdullahbkhamis@gmail.com>
2026-02-07 11:56:30 -0500

    setup cors plugin for elysia

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,17 @@
+import cors from "@elysiajs/cors";
 import { Elysia } from "elysia";
+import { CLIENT_URL } from "../constants/env.constants";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(
+		cors({
+			origin: [CLIENT_URL],
+			methods: ["POST", "PATCH", "GET", "DELETE"],
+		}),
+	)
+	.get("/", () => "Hello Elysia")
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
